689d2f94d6d6e138243b145158823dec92ebc8cf
Bradley Wojcik <bradleywojcik@gmail.com>
2019-12-11 19:27:21 -0500

    made navbar adaptive on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'site';
+
+  @ViewChild('navBurger', { static: true }) navBurger: ElementRef;
+  @ViewChild('navMenu', { static: true }) navMenu: ElementRef;
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
+  }
 }
